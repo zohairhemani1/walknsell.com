650d39497fa4d131a45529c54f31a625c604939d
zohairHemani1 <zohairhemani1@gmail.com>
2014-08-18 13:19:30 +0500

    Activation News Letter

---
+++ newsletters/activation.php
@@ -0,0 +1,92 @@
+<?php
+
+$to = "zainshafimemon@gmail.com";
+$subject = "WalkNSell: Activate Your New Account";
+$headers = "From: " . "info@walknsell.com" . "\r\n";
+$headers .= "Reply-To: ". "info@walknsell.com" . "\r\n";
+$headers .= "CC: zohairhemani1@gmail.com\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+$message = "<table cellspacing='0' cellpadding='0' style='padding:30px 10px;background:#eee;width:100%;font-family:arial'>
+			  <tbody>
+				<tr>
+				  <td><table align='center' cellspacing='0' style='max-width:650px;min-width:320px'>
+					  <tbody>
+						<tr>
+						  <td style='text-align:left;padding-bottom:14px'><img align='left' alt='Fiverr' src='https://ci5.googleusercontent.com/proxy/aHAvPRQ40UGPkcIi9S-UF0YMlSHhr_420DfoVQjfJ6kKQEgABq0C_4x6KRw7u8LkEBAOABKOuiqXfEAEmmCX4ezn7dE62U9tA6-XtJuZJ-JrPDZcq5PSOS5e=s0-d-e1-ft#http://d2nt7j7ljjsiah.cloudfront.net/assets/v2_emails/email_logo.png'></td>
+						</tr>
+						<tr>
+						  <td align='center' style='background:#fff;border:1px solid #e4e4e4;padding:50px 30px'><div>
+							  <div>
+								<div> </div>
+							  </div>
+							</div>
+							<table align='center'>
+							  <tbody>
+								<tr>
+								  <td style='border-bottom:1px solid #dfdfd0;color:#666;text-align:center;padding-bottom:30px'><table align='center' style='margin:auto'>
+									  <tbody>
+										<tr>
+										  <td style='color:#666;font-size:20px;font-weight:bold;text-align:center;font-family:arial'> Hi zainumemon, Welcome to Fiverr! </td>
+										</tr>
+									  </tbody>
+									</table>
+									<table align='center' style='margin:auto'>
+									  <tbody>
+										<tr>
+										  <td style='color:#666;font-size:16px;padding-bottom:30px;text-align:center;font-family:arial'> Are you ready to get started? </td>
+										</tr>
+									  </tbody>
+									</table>
+									<table align='center' style='margin:auto'>
+									  <tbody>
+										<tr>
+										  <td style='background-color:#00b22d;border:1px solid #028a25;border-radius:3px;text-align:center'><a href='http://www.fiverr.com/activate/uzljNhQJYI0b6fsROcgZ?cr=53f1b35f&amp;utm_medium=d001&amp;utm_source=el_cn&amp;utm_term=na_na' style='padding:16px 20px;display:block;text-decoration:none;color:#fff;font-size:16px;text-align:center;font-family:arial;font-weight:bold' target='_blank'>ACTIVATE YOUR ACCOUNT</a></td>
+										</tr>
+									  </tbody>
+									</table></td>
+								</tr>
+								<tr>
+								  <td style='color:#aaa;padding:15px;font-size:11px;line-height:15px;text-align:left'><div style='color:#aaa;padding-bottom:15px;font-family:arial'> If clicking the link above does not work, copy and paste the following URL in a new browser window: <a href='http://www.fiverr.com/activate/uzljNhQJYI0b6fsROcgZ?cr=53f1b35f&amp;utm_medium=d001&amp;utm_source=el_cn&amp;utm_term=na_na' style='color:#00b22c' target='_blank'>http://www.fiverr.com/<wbr>
+									  activate/uzljNhQJYI0b6fsROcgZ?<wbr>
+									  cr=53f1b35f&amp;utm_medium=d001&amp;<wbr>
+									  utm_source=el_cn&amp;utm_term=na_<wbr>
+									  na</a> </div>
+									<div style='color:#aaa;font-family:arial'> It is also a good idea to add <a style='color:#aaa'>noreply@e.fiverr.com</a> to your address book to ensure that you receive our messages (no spam, we promise!). </div></td>
+								</tr>
+							  </tbody>
+							</table></td>
+						</tr>
+						<tr>
+						  <td align='center' style='background:#f8f8f8;border:1px solid #e4e4e4;border-top:none;padding:24px 10px'><table align='center' style='width:100%;max-width:650px'>
+							  <tbody>
+								<tr>
+								  <td style='text-align:center'><a href='http://app.appsflyer.com/id346080608?pid=Organic&amp;c=Emails_System' style='text-decoration:none;margin-bottom:10px;display:inline-block' target='_blank'> <img width='208' height='40' border='0' src='https://ci4.googleusercontent.com/proxy/M_xRJOsxVu5vjZUDPUoNXxNeTRuJ_Uxl6VtV08YA445t4a-tL5o9O988ei5aF4H3LSuaxHBBMDPGX38sXKDS8iFTRcEL-8bv6-Lhr6r9TwF2pVPVl4cGEXPSHKc_rRuJZgDQSou84g4i7KCAxy3PCsUirec=s0-d-e1-ft#https://d7l5bbi2x5xo4.cloudfront.net/cloud_files/974/original/email-footer-app-store-space.gif'> </a> <a href='http://app.appsflyer.com/com.fiverr.fiverr?pid=Organic&amp;c=Emails_System' style='text-decoration:none;display:inline-block' target='_blank'> <img width='208' height='40' border='0' src='https://ci5.googleusercontent.com/proxy/iHE7QUvx2J2u1njfDH8CCQJrx7-pX2SzKOkUqEeTf6wQ80DUjPnm8jQlxB7be9hRgqT0SJ-F6Gzo5iShZzjDtVHXMVArxbJcZaBbkLZAxQ-j7En1gNki1BQnrM91_5LaOvMgFACWEhZ1pOe6LF898JdsuGkz=s0-d-e1-ft#https://d7l5bbi2x5xo4.cloudfront.net/cloud_files/975/original/email-footer-play-store-space.gif'> </a></td>
+								</tr>
+								<tr>
+								  <td style='font-size:20px;line-height:27px;text-align:center;max-width:650px'><a href='http://www.fiverr.com/ios_app?from_email=true' style='text-decoration:none;color:#69696c' target='_blank'> <span style='color:#00ce00;font-weight:bold;max-width:180px'>NEW!</span> All the Gigs<sup style='font-size:14px'>®</sup> You Need <br>
+									Now in Your Pocket </a></td>
+								</tr>
+							  </tbody>
+							</table></td>
+						</tr>
+					  </tbody>
+					</table></td>
+				</tr>
+				<tr>
+				  <td><table align='center' style='max-width:650px'>
+					  <tbody>
+						<tr>
+						  <td style='color:#b4b4b4;font-size:11px;padding-top:10px;line-height:15px;font-family:arial'> © Fiverr International Limited 2010-2013, FIVERR®, GIG®, GIGS®, I WILL__for$5, The Things People Do For $5, DamnQuickPay, Fiverr graphics, logos, scripts, terms of use, instructions, designs and others service names are the trademarks and copyright of Fiverr International Limited. Fiverr® is located at 460 Park Avenue, New York, NY10016. <br>
+							Apple, the Apple logo and iPhone are trademarks of Apple Inc., registered in the U.S. and other countries. App Store is a service mark of Apple Inc. </td>
+						</tr>
+					  </tbody>
+					</table></td>
+				</tr>
+			  </tbody>
+			</table>";
+
+mail($to, $subject, $message, $headers);
+
+?>
